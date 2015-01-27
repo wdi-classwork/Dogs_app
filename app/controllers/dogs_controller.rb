ca9f9ce36cd62c589fb80ceea674fb996d480f5e
Helen Thi <ht5658@gmail.com>
2015-01-27 11:08:00 -0500

    added new and create methods with new form_for in view; also added links for navigation

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,4 +2,18 @@ class DogsController < ApplicationController
   def index
     @dogs = Dog.all
   end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.create(dog_params)
+    redirect_to action: :index
+  end
+
+  private
+  def dog_params
+    params.require(:dog).permit(:name, :gender, :age, :breed, :vaccinated?)
+  end
 end
